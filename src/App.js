a1ddcf436bd1cb100a4e014c0649697dc0814393
Swapna135 <tswapna135@gmail.com>
2022-05-12 04:16:26 +0000

    Lists & Keys

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './Components/UserProfile/index'
+
+const UserDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Esther Horward',
+  role: 'Software Developer',
+}
+
+const App = () => <UserProfile />
+
+export default App
